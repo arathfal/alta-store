b704ea401a20bb83c7754bf00d4a5bd4c06cb1b9
Aradea Atfal Risdianto <aradeaa9@gmail.com>
2021-06-24 15:49:02 +0700

    Update CustomerModel

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,5 @@ func New() *echo.Echo {
 	e := echo.New()
 	e.GET("/customers", controllers.GetCustomerController)
 	e.POST("/customers/register", controllers.RegisterController)
-	e.DELETE("carts/:cartId/products/:productsId", controllers.DeleteCartControllers)
 	return e
 }
