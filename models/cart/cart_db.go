02dc8f11a948e544db4198912f3a6b22523cb10e
dikanp <andika.dikanp@gmail.com>
2021-06-26 06:57:29 +0700

    menambahkan json di model

--- models/cart/cart_db.go
+++ models/cart/cart_db.go
@@ -4,6 +4,6 @@ import "AltaStore/models"
 
 type Cart struct {
 	models.GormModel
-	Products   []Product `gorm:"many2many:cart_products;"`
-	CustomerID uint
+	Products   []Product `gorm:"many2many:cart_products;" json:"product"`
+	CustomerID 	uint	`json:"customer_id"`
 }
