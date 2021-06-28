77138290996b97863dea86a8f307ff5346231ac4
Aradea Atfal Risdianto <aradeaa9@gmail.com>
2021-06-28 20:14:40 +0700

    Update Register Branch

---
+++ configs/config.go
@@ -0,0 +1,44 @@
+package configs
+
+import (
+	"AltaStore/models/customer"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// type config struct {
+// 	DB_
+// }
+
+func InitDB() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	username := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	host := os.Getenv("HOST")
+	schema := os.Getenv("SCHEMA")
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s",
+		username, password, host, schema,
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dataSourceName), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+	AutoMigrate()
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(&customer.Customer{})
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
