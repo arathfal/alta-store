d1eca430f49f1692b5159b01c24a6a6ac254e412
dikanp <andika.dikanp@gmail.com>
2021-06-29 17:35:49 +0700

    delete product di cart

--- models/product/product_db.go
+++ models/product/product_db.go
@@ -1,10 +1,12 @@
 package product
 
-import "AltaStore/models"
-
+import (
+	"AltaStore/models"	
+)
 type Product struct {
 	models.GormModel
-	Name        string	`json:"name"`
-	Stock       int			`json:"stock"`
-	Description string	`json:"desciption"`
+	Name        string `json:"name"`
+	Stock       int    `json:"stock"`
+	Description string `json:"description"`
+	CategoryID 	int			
 }
