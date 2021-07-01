65f89c2948c3be0675a33764e13bfa89a2b11464
dikanp <andika.dikanp@gmail.com>
2021-07-01 13:47:20 +0700

    hooks after create customer, create cart

--- models/customer/customer_db.go
+++ models/customer/customer_db.go
@@ -4,6 +4,8 @@ import (
 	"AltaStore/models"
 	"AltaStore/models/cart"
 	"AltaStore/models/order"
+
+	"gorm.io/gorm"
 )
 
 type Customer struct {
@@ -13,4 +15,13 @@ type Customer struct {
 	Password string `json:"password"`
 	Cart     cart.Cart
 	Order    order.Order
+}
+
+
+func (c *Customer) AfterCreate(tx *gorm.DB) (err error) {
+	// tx.Model(c).Update(, "admin")
+	var cart cart.Cart
+	cart.CustomerID = c.ID
+	tx.Save(&cart)
+  return
 }
